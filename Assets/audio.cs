15c55cfe65ea375969473b6c114aff693a7fd2b8
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-05 16:12:00 +0900

    thirdcommit

---
+++ Assets/audio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audio : MonoBehaviour {
+
+	public AudioSource sound01;
+
+	void Start () {
+		//AudioSourceコンポーネントを取得し、変数に格納
+		sound01 = GetComponent<AudioSource>();
+		sound01.PlayOneShot(sound01.clip);
+	}
+
+	void Update () {
+		//指定のキーが押されたら音声ファイル再生
+
+		
+	}
+}
