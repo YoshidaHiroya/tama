4417c1dec78520e796bf811f0e934f2fb5686eb1
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-06 21:31:04 +0900

    thirdcommit

---
+++ Assets/enemycontroller1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemycontroller1 : MonoBehaviour {
+	public int speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += new Vector3 (speed*Time.deltaTime,0,0);
+		if(this.transform.position.x>25.0f){
+			Destroy (gameObject);
+	}
+}
+}
---
+++ Assets/enemymanager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymanager : MonoBehaviour {
+	float time;
+	public GameObject enemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time > 1.0f) {
+			Instantiate (enemy, new Vector3(Random.Range(0,20),0,0) , Quaternion.identity);
+			time = 0.0f;
+		}
+
+	}
+}
